a9ca25d8fbc0a0812f40781968a72bcace345e65
Dmitrijs1984 <siricad@gmail.com>
2018-07-14 13:49:24 +0300

    1)ctrl+Alt+L 2) only one scanner

--- src/com/RandomNumberGenerator.java
+++ src/com/RandomNumberGenerator.java
@@ -1,8 +1,8 @@
 package com;
 
 public class RandomNumberGenerator {
-    public long generateRandomNumber(long randomNumbersRightBorder, long randomNumbersLeftBorder){
-       long randomNumber = (long)(Math.random()*((randomNumbersRightBorder-randomNumbersLeftBorder)+1))+randomNumbersLeftBorder;
+    public long generateRandomNumber(long randomNumbersRightBorder, long randomNumbersLeftBorder) {
+        long randomNumber = (long) (Math.random() * ((randomNumbersRightBorder - randomNumbersLeftBorder) + 1)) + randomNumbersLeftBorder;
         return randomNumber;
     }
 }
